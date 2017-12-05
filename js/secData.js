ca57b8c18f4a0a2c609de93aca4ec6a4e2a50166
liuqi <754493376@qq.com>
2017-12-05 09:46:47 +0800

    进一步优化

--- js/secData.js
+++ js/secData.js
@@ -2998,6 +2998,7 @@
                 axisPointer : {            // 坐标轴指示器，坐标轴触发有效
                     type : 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
                 }
+
             },
             grid: {
                 left: '3%',
